6f38227f4a184ea951621dc06bbc645e1d3e3e97
Stefan Åhlander <cs.ahlander@gmail.com>
2021-02-08 11:09:32 +0100

    add how-sum alg

---
+++ how-sum.js
@@ -0,0 +1,23 @@
+const howSum = (targetSum, numbers, memo = {}) => {
+  if (targetSum in memo) return memo[targetSum];
+  if (targetSum === 0) return [];
+  if (targetSum < 0) return null;
+
+  for (let num of numbers) {
+    const remainder = targetSum - num;
+    const remainderResult = howSum(remainder, numbers, memo);
+    if (remainderResult !== null) {
+      memo[targetSum] = remainderResult.concat(num);
+      return memo[targetSum];
+    }
+  }
+
+  memo[targetSum] = null;
+  return null;
+};
+
+console.log(howSum(7, [2, 3]));
+console.log(howSum(7, [5, 3, 4, 7]));
+console.log(howSum(7, [2, 4]));
+console.log(howSum(8, [2, 3, 5]));
+console.log(howSum(300, [7, 14]));
